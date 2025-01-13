ee0d124940e56f256285bbebf8172faafdfccbef
Ali Aghamirbabaei <a.aghamirbabaei@nizek.com>
2025-01-13 17:51:43 +0330

    Completed Building list and navigations.

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ali Aghamirbabaei on 1/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
